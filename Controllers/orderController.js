76f6797ece0bd986ab27f4508f549159ced6b071
Gonçalo Maciel <goncalomaciel2250@gmail.com>
2023-12-15 18:24:36 +0000

    orderCreate update chat

--- Controllers/orderController.js
+++ Controllers/orderController.js
@@ -26,6 +26,7 @@ exports.getOrderById = async (req, res) => {
   }
 };
 
+/*
 exports.createOrder = async (req, res) => {
   try {
     const newOrder = new Order(req.body);
@@ -82,7 +83,94 @@ exports.createOrder = async (req, res) => {
     res.status(500).json({ status: 500, message: "Error creating order", data: {} });
   }
 };
+*/
 
+exports.createOrder = async (req, res) => {
+  try {
+    // Calcular o total com base nos itens do pedido
+    const orderTotal = req.body.items.reduce((acc, item) => {
+      // Certifique-se de que item.quantity e item.productId estão definidos
+      if (item.quantity && item.productId) {
+        // Obtenha o preço do produto (você pode precisar modificar isso com base na estrutura do seu modelo de produto)
+        const product = await axios.get(
+          `http://${process.env.PRODUCTS_URI}:8083/products/GetProduct/${item.productId}`,
+          {
+            headers: {
+              Authorization: req.headers.authorization,
+            },
+          }
+        );
+        const productPrice = product.data.price || 0;
+
+        // Adicione ao total
+        acc += item.quantity * productPrice;
+      }
+      return acc;
+    }, 0);
+
+    // Crie a nova ordem com o total calculado
+    const newOrder = new Order({
+      ...req.body,
+      total: orderTotal,
+    });
+
+    const savedOrder = await newOrder.save();
+    const token = req.headers.authorization;
+
+    const updateStockPromises = req.body.items.map(async (item) => {
+      const quantityUsed = item.quantity || 1;
+      const { productId, serviceId } = item;
+      try {
+        if (productId) {
+          const stock = await axios.post(
+            `http://${process.env.PRODUCTS_URI}:8083/stock/EditStock`,
+            {
+              ProductID: productId,
+              newQuantity: quantityUsed,
+            },
+            {
+              headers: {
+                Authorization: token,
+              },
+            }
+          );
+          return stock.data;
+        } else if (serviceId) {
+          const service = await axios.get(
+            `http://${process.env.SERVICES_URI}:8084/service/ReadService/`,
+            {
+              params: {
+                serviceID: serviceId,
+              },
+            }
+          );
+          if (!service.data || !service.data.service) {
+            throw new Error("Service not found");
+          }
+          return service.data.service;
+        } else {
+          throw new Error("Invalid item in the order");
+        }
+      } catch (error) {
+        console.error(error);
+        throw new Error("Error updating products stock");
+      }
+    });
+
+    const updatedStocks = await Promise.all(updateStockPromises);
+
+    res.json({
+      status: 200,
+      message: "Order created",
+      data: { order: savedOrder, stocks: updatedStocks },
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ status: 500, message: "Error creating order", data: {} });
+  }
+};
 
 exports.updateOrder = async (req, res) => {
   try {
